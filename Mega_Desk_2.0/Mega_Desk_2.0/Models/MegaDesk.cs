ed9232c7f9f16d97a46e86061a41a37c3f96872e
jambrizz <ambrizjovani@gmail.com>
2023-11-01 17:32:03 -0700

    Creation of Mega Desk 2.0

---
+++ Mega_Desk_2.0/Mega_Desk_2.0/Models/MegaDesk.cs
@@ -0,0 +1,15 @@
+﻿namespace Mega_Desk_2._0.Models
+{
+    public class MegaDesk
+    {
+        public int ID { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public int Width { get; set; }
+        public int Depth { get; set; }
+        public int Drawers { get; set; }
+        public string SurfaceMaterial { get; set; }
+        public int RushOrder { get; set; }
+        public double Cost { get; set; }
+    }
+}
